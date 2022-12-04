2ee0e1e52fa790e636febe790efaff4c664bbd47
Dich <dichchok@gmail.com>
2022-12-04 13:18:02 +0300

    hide statusbar launchscreen and start status are white

--- TheLight/ViewController.swift
+++ TheLight/ViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var isLightOn: Bool = false
+    var isLightOn: Bool = true
+    override var prefersStatusBarHidden: Bool{
+        return true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
