e3c97291768a017c830cea5a55a587f396164ea2
Dich <dichchok@gmail.com>
2022-12-11 02:44:05 +0300

    Add colors by press on screen 1 2 3 4 red (default) green white black - on black add function from AVFoundation classes AVCaptureDevice torch mode on/off (torch/flashlight)

--- TheLight/ViewController.swift
+++ TheLight/ViewController.swift
@@ -6,9 +6,10 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-    var isLightOn: Bool = true
+    var isLightOn: Int = 1
     override var prefersStatusBarHidden: Bool{
         return true
     }
@@ -17,14 +18,50 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         updateUI()
     }
+    func toggleTorch(on: Bool) {
+        guard let device = AVCaptureDevice.default(for: .video) else { return }
 
+        if device.hasTorch {
+            do {
+                try device.lockForConfiguration()
+
+                if on == true {
+                    device.torchMode = .on
+                } else {
+                    device.torchMode = .off
+                }
+
+                device.unlockForConfiguration()
+            } catch {
+                print("Torch could not be used")
+            }
+        } else {
+            print("Torch is not available")
+        }
+    }
 
     fileprivate func updateUI() {
-        view.backgroundColor = isLightOn ? .white : .black
+        //view.backgroundColor = isLightOn ? .white : .black
+        switch isLightOn {
+        case 1:
+            view.backgroundColor = .red
+            toggleTorch(on: false)
+        case 2:
+            view.backgroundColor = .green
+            toggleTorch(on: false)
+        case 3:
+            view.backgroundColor = .white
+            toggleTorch(on: false)
+        default:
+            view.backgroundColor = .black
+            toggleTorch(on: true)
+        }
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        isLightOn.toggle()
+        print(isLightOn)
         updateUI()
+        if (isLightOn > 3) {isLightOn = 1}
+        else {isLightOn+=1}
     }
 }
 
